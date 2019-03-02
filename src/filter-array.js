a27ee5407bde58e58d6af7415970f02dbfab4597
Tommy Tran <tranttommy95@gmail.com>
2019-03-01 23:30:59 -0800

    refactored parameter names

--- src/filter-array.js
+++ src/filter-array.js
@@ -1,7 +1,7 @@
-export default function filterArray(testArray, filterChoices) {
-   const filtered = testArray.filter(pokemon => {
-      const type = filterChoices.type === 'all' ? true : filterChoices.type === pokemon.type_1 || filterChoices.type === pokemon.type_2;
-      const region = !filterChoices.region ? true : filterChoices.region === pokemon.generation_id;
+export default function filterArray(array, filter) {
+   const filtered = array.filter(pokemon => {
+      const type = filter.type === 'all' ? true : filter.type === pokemon.type_1 || filter.type === pokemon.type_2;
+      const region = !filter.region ? true : filter.region === pokemon.generation_id;
       return type && region;
    });
    return filtered;
