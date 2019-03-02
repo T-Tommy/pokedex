6d2b3cd673e0e0a5f56431dceb485949c88f98df
Tommy Tran <tranttommy95@gmail.com>
2019-03-01 23:31:55 -0800

    added perPage event listener and refactored updatePageNav

--- src/paging-component.js
+++ src/paging-component.js
@@ -2,15 +2,23 @@ const prevButton = document.getElementById('previous');
 const nextButton = document.getElementById('next');
 const currentPage = document.getElementById('current-page');
 const totalPages = document.getElementById('total-pages');
+const perPageSelect = document.getElementById('per-page-select');
 
 let currentPageNumber = 1;
 currentPage.textContent = currentPageNumber;
 
 export default function changePage(listLength, callback) {
-   const PER_PAGE = 25;
-   let totalPagesNumber = Math.ceil(listLength / PER_PAGE);
+
+   let perPageNumber = Number(perPageSelect.value);
+   let totalPagesNumber = Math.ceil(listLength / perPageNumber);
    totalPages.textContent = totalPagesNumber;
    updatePageNav();
+
+   perPageSelect.addEventListener('change', () => {
+      currentPageNumber = 1;
+      perPageNumber = Number(perPageSelect.value);
+      updatePageNav();
+   });
    nextButton.addEventListener('click', () => {
       currentPageNumber++;
       updatePageNav();
@@ -21,12 +29,19 @@ export default function changePage(listLength, callback) {
    });
 
    function updatePageNav() {
-      prevButton.disabled = currentPageNumber === 1;
-      nextButton.disabled = currentPageNumber === totalPagesNumber;
+      totalPagesNumber = Math.ceil(listLength / perPageNumber);
+      totalPages.textContent = totalPagesNumber;
+      prevButton.disabled = currentPageNumber <= 1;
+      if(currentPageNumber >= totalPagesNumber) {
+         nextButton.disabled = true;
+         currentPageNumber = totalPagesNumber;
+      } else {
+         nextButton.disabled = false;
+      }
       currentPage.textContent = currentPageNumber;
       const pageInfo = {
          currentPage: currentPageNumber,
-         perPage: PER_PAGE
+         perPage: perPageNumber
       };
       callback(pageInfo);
    }
